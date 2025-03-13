83b408db00aaa7221aaec768383426df4b064d88
rjbtutor <rjbtutor@gmail.com>
2025-03-13 14:31:08 +0000

    fixed compiler error

--- gallery/src/button.tsx
+++ gallery/src/button.tsx
@@ -1,12 +1,9 @@
 export default function Button() {
-  const isNode = () =>
-    // typeof process !== 'undefined' &&
-    // !!process.versions &&
-    // !!process.versions.node;
-  let dom = 'unknown domain';
-  if (true) {
-    dom = `${import.meta.env.PRODUCTION_DOMAIN}`;
-  }
+  // const isNode = () =>
+  // typeof process !== 'undefined' &&
+  // !!process.versions &&
+  // !!process.versions.node;
+  const dom = `${import.meta.env.PRODUCTION_DOMAIN}`;
 
   return <div style={{ background: 'red' }}>Domain=[{dom}]</div>;
 }
